dd378092d801ea1f706fcae1dfc94bb6b9d7d7cb
Rahul123hazarika <98834787+Rahul123hazarika@users.noreply.github.com>
2025-09-17 09:07:47 +0530

    Create js_ecmascript_problem3.js

---
+++ js_ecmascript_problem3.js
@@ -0,0 +1,11 @@
+
+// // You're building a user profile system. Implement a funcƟon that merges a
+// user's basic info with their preferences into a single object.
+
+const profile=(userinfo, userpreference)=>{
+    return (userinfo, userpreference);
+    
+}
+const userinfo={name: "rahul", phone:903433433, email:"rahulhazarika612@gmail.com"};
+const userpreference={theme:"dark", lanuage:"en"};
+console.log(profile(userinfo, userpreference));
