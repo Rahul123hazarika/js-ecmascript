4b40d7e01ed6c4a1e19c45126a93ae722be2c9ed
Rahul123hazarika <98834787+Rahul123hazarika@users.noreply.github.com>
2025-09-17 09:12:12 +0530

    Create js_ecmascript_problem4.js

---
+++ js_ecmascript_problem4.js
@@ -0,0 +1,5 @@
+//In a messaging app, you want to display only the first name of a user.
+//Create a funcƟon that extracts the first name from a full name string. 
+const displayname=(username)=>{
+  
+}
